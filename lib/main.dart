cd62fbb01919355d6a60a14f95d7535de0fab92e
SachaDodane <sachado21@gmail.com>
2024-06-10 15:24:35 +0200

    finEtape6

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,13 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list_v1/todo_list_app.dart';
 import 'package:todo_list_v1/models/task_provider.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: 'assets/.env');
   await Supabase.initialize(
-    url: 'https://cskyvytztbvghpiloibi.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNza3l2eXR6dGJ2Z2hwaWxvaWJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc0OTE4ODgsImV4cCI6MjAzMzA2Nzg4OH0.LiI3cyhLzwdVaVJPx__v6kWaaITSFdqZSk8hAMUcBGE',
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_API_KEY']!,
   );
   runApp(const ToDoListApp());
 }
